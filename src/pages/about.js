25ed3e8322e470d970b08eb9a14b952dd973225f
Uuhuuu <pawelajewski@gmail.com>
2022-11-09 15:12:31 +0100

    0-11 Snippets prettier

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+ 
+ const About = () => {
+   return (
+     <div>
+        <h1>This is the about page</h1>
+        <p>imr for import React from "react"</p>
+        <p>rafc for react component</p>
+     </div>
+   )
+ }
+ 
+ export default About;
